6f3de510647a7392b6275d63f0d1cdfb46184b97
Anne-So <anne.sophie.koch54@gmail.com>
2024-10-26 17:35:01 +0100

    fix bottom right buttons + animal rotation

--- Assets/Scripts/AI/WanderingThings.cs
+++ Assets/Scripts/AI/WanderingThings.cs
@@ -8,20 +8,17 @@ public class WanderingThings : MonoBehaviour
     public float speed = 10;
     public List<Transform> waypoints;
     public bool stop = false;
+    public Vector3 size;
     
     private int waypointIndex;
     
-
     private float minDistance = 0.25f;
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+	private void Start()
+	{
+        size = transform.localScale;
+	}
+	void Update()
     {
         if(stop || waypoints.Count == 0)return;
         
@@ -31,6 +28,8 @@ public class WanderingThings : MonoBehaviour
         
         if ((transform.position - tr.position).sqrMagnitude < minDistance * minDistance)
         {
+            int oldwaypoint = waypointIndex;
+
             if (repeat)
             {
                 waypointIndex = (waypointIndex + 1) % waypoints.Count;
@@ -42,6 +41,16 @@ public class WanderingThings : MonoBehaviour
                 {
                     stop = true;
                 }
+            }
+
+            //déplacement gauche/droite
+            if (waypoints[waypointIndex].position.x > waypoints[oldwaypoint].position.x)
+            {
+				transform.localScale = size; // droite
+            }
+            else
+            {
+				transform.localScale = new Vector3(size.x*-1, size.y, 1); // gauche
             }
         }
     }
